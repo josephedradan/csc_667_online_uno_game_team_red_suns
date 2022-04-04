aa26373191fa2f8d7ba483a7a3749c1002af3d7d
Eddy Yun <eyun1@mail.sfsu.edu>
2022-04-03 23:13:09 -0700

    add game lobby

--- routes/games.js
+++ routes/games.js
@@ -5,4 +5,8 @@ router.get("/", function (req, res, next) {
     res.render("pregame_lobby", { title: "Pre-game Lobby" });
 });
 
+router.get("/testGame", function (req, res, next) {
+    res.render("game_lobby", { title: "Game Lobby" });
+});
+
 module.exports = router;
