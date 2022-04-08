65e4ea9e36c23adf9fbde72da8cfa25ddf33e1e9
jsanjose-sfsu <jsanjose@mail.sfsu.edu>
2022-04-08 15:35:28 -0700

    registration logic added, clients can now register accounts however, usernames are not unique unfortunately

--- controller/controller_users.js
+++ controller/controller_users.js
@@ -1,3 +1,6 @@
+const db = require("../db");
+const bcrypt = require("bcrypt"); 
+
 controllerUsers = {}
 
 function sendUsers(req, res, next) {
@@ -5,10 +8,59 @@ function sendUsers(req, res, next) {
 }
 
 function registerUser(req, res, next) {
-    console.log("Hi"); 
+    console.log("-------In controller_user.registerUser()-------"); 
     console.log("req:");
     console.log(req.body);  
-    res.render("index"); 
+    const {username, password, confirm_password} = req.body; 
+
+    //backend validation;
+    const regExpUsername = /^(?![_ -])(?:(?![_ -]{2})[\w -]){5,16}(?<![_ -])$/;
+    const regExpPassword = /^(?:(?=.*?\p{N})(?=.*?[\p{S}\p{P} ])(?=.*?\p{Lu})(?=.*?\p{Ll}))[^\p{C}]{8,16}$/; 
+
+    if(username.match(regExpUsername) === null) {
+        //TODO: add frontend alert messages to the client. 
+        console.log("Improper username, follow 'https://stackoverflow.com/questions/46453307/the-ideal-username-and-password-regex-validation'"); 
+        res.render("registration"); 
+    } else if(password.match(regExpPassword) === null && password!== confirm_password) {
+        //TODO: add frontend alert messages to the client. 
+        console.log("Improper password, follow 'https://stackoverflow.com/questions/46453307/the-ideal-username-and-password-regex-validation'");
+        res.render("registration");
+    } else {
+
+
+        function insertUser(username, password) {
+            //TODO: insert user into the database. 
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(password, salt, async (err, hash) => {
+                    if(err) {
+                        console.log("err in hashing"); 
+                        return; 
+                    }
+                    // Store hash in your password DB.
+                    console.log(username + ":" + hash + ":" + password);
+                    await db.any(
+                        `
+                        INSERT INTO public."Account"(
+                            username, password)
+                        VALUES ('${username}', '${hash}');
+                
+                        INSERT INTO public."Account Statistics"(
+                            "num_Wins", "num_Loss")
+                        VALUES (0, 0);
+                        `
+                    )
+
+
+                });
+            })
+        }
+        
+        insertUser(username, password); 
+        res.render("index"); 
+
+    }
+
 }
 
 controllerUsers.sendUsers = sendUsers; 
