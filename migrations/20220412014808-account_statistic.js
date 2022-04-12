95aa797e5412e1bc37c9fe1a8655f521ebff17db
jsanjose-sfsu <jsanjose@mail.sfsu.edu>
2022-04-11 19:12:39 -0700

    completed account-statistic table migration (untested)

--- migrations/20220412014808-account_statistic.js
+++ migrations/20220412014808-account_statistic.js
@@ -8,6 +8,38 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    return queryInterface.createTable("AccountStatistic", {
+
+      statistic_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      }, 
+
+      num_wins: {
+        type: Sequelize.INTEGER, 
+        defaultValue: 0
+      }, 
+
+      num_loss: {
+        type: Sequelize.INTEGER, 
+        default: 0
+      }, 
+
+
+      date_joined: {
+        type: Sequelize.DATE, 
+        defaultValue: Sequelize.literal("NOW()"),
+        allowNull: false
+      }, 
+
+      account_id: {
+        type: Sequelize.INTEGER, 
+        references: { model: 'Account', key: 'account_id' }, 
+        unique: true
+      }
+
+    }); 
   },
 
   async down (queryInterface, Sequelize) {
@@ -17,5 +49,6 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
+    return queryInterface.dropTable("AccountStatistic"); 
   }
 };
