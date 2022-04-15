f3c96ce9e9a555db88d2616289e309590559a7f9
jsanjose-sfsu <jsanjose@mail.sfsu.edu>
2022-04-14 21:49:42 -0700

    finished lobby

---
+++ migrations/20220415044236-lobby.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+     return await queryInterface.createTable("Lobby", {
+      
+      lobby_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      }, 
+
+      is_in_game: {
+        type: Sequelize.BOOLEAN, 
+        default: false
+      }, 
+
+      current_player_id: {
+        type: Sequelize.INTEGER, 
+        references: { model: 'Player', key: 'player_id' }
+      },
+
+      host_id: {
+        type: Sequelize.INTEGER, 
+        allowNull: false, 
+        references: { model: 'Player', key: 'player_id' }
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return await queryInterface.dropTable('Lobby');
+  }
+};
