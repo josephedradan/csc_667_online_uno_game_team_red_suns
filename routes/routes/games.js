1dac9eb24f2f1be2e5119b791b087de2b12de63c
jsanjose-sfsu <jsanjose@mail.sfsu.edu>
2022-04-20 10:35:02 -0700

    applying joseph's backend guard validation to prevent unauthenticated/not logged in users from entering a game.

--- routes/routes/games.js
+++ routes/routes/games.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const routerGames = express.Router();
 
-const controllerGames = require("../../controller/controller_games")
+const controllerGames = require("../../controller/controller_games"); 
+const middlewareAuthenticationPassport = require("../../middleware/middleware_authentication_passport");
 
-routerGames.get("/", controllerGames.renderPreGameLobby);
+routerGames.get("/", 
+    middlewareAuthenticationPassport.checkAuthenticated, 
+    controllerGames.renderPreGameLobby);
 
-routerGames.get("/renderTestGame", controllerGames.renderTestGame);
+routerGames.get("/renderTestGame", 
+    middlewareAuthenticationPassport.checkAuthenticated, 
+    controllerGames.renderTestGame);
 
 module.exports = routerGames;
