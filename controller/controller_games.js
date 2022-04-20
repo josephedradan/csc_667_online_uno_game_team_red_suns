1058557041ed386204c070ad2130c5b350ac9eaf
Eddy Yun <eyun1@mail.sfsu.edu>
2022-04-20 09:17:28 -0700

    combine pre-game with game lobby (rename to lobby.hbs), edit game controller to lobby, add additional rule in header

--- controller/controller_games.js
+++ controller/controller_games.js
@@ -1,14 +1,13 @@
-const controllerGames = {}
+const controllerGames = {};
 
-controllerGames.renderPreGameLobby = (req, res, next) =>{
-    console.log("MAIN TSET JOSEPH")
-    res.render("pregame_lobby", {title: "Pre-game Lobby"});
-}
+controllerGames.renderPreGameLobby = (req, res, next) => {
+    console.log("MAIN TSET JOSEPH");
+    res.render("lobby", { title: "Lobby" });
+};
 
+controllerGames.renderTestGame = (req, res, next) => {
+    console.log("TEST GAME");
+    res.render("game_lobby", { title: "Game Lobby" });
+};
 
-controllerGames.renderTestGame = (req, res, next) =>{
-    console.log("TEST GAME")
-    res.render("game_lobby", {title: "Game Lobby"});
-}
-
-module.exports = controllerGames
+module.exports = controllerGames;
