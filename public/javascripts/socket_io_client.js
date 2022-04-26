b6289025c2d8a09fd30667cd0a95d6bc495b270c
jsanjose-sfsu <jsanjose@mail.sfsu.edu>
2022-04-26 14:04:00 -0700

    emitting game_id through the websocket via window object.

--- public/javascripts/socket_io_client.js
+++ public/javascripts/socket_io_client.js
@@ -1,12 +1,22 @@
 // eslint-disable-next-line no-var,no-undef
 var socket = io();
-
+const LOCALHOST_SIZE = 'http://localhost:3000/'.length;
 socket.on('connect', () => {
     console.log(`Connected, your id is: ${socket.id}`);
 
     // THIS MUST BE CALLED ONCE AND BEFORE ANY OTHER EVENTS
-    socket.emit('client-join-room'); // parse for game_id from URL.
+    const currPath = window.location.href.slice(LOCALHOST_SIZE);
+    console.log(currPath);
+
+    const gameRE = '(?:game)';
+    const containsGame = currPath.search(gameRE);
+    console.log(`contains game: ${containsGame > -1}`);
+    if (containsGame > -1) {
+        const gameId = currPath.split('/')[1];
+        console.log(`game_id: ${gameId}`);
+        socket.emit('client-join-room', gameId); // parse for game_id from URL.
     // TODO: PARSE URL Using Window object, review regex to grab game_id, verify if youre in the game lobby
+    }
 
     // Test messages
     socket.emit('client-message', 'Hello 1');
