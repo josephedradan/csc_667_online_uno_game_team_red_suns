ae6d7aa1fee649d7045142c40f0699f8d0fd85ba
Eddy Yun <eyun1@mail.sfsu.edu>
2022-04-26 19:24:20 -0700

    socket msg done

--- public/javascripts/socket_io_client.js
+++ public/javascripts/socket_io_client.js
@@ -1,7 +1,5 @@
 // eslint-disable-next-line no-var,no-undef
 var socket = io();
-const message_sandbox = document.getElementById("sandbox_message_box");
-
 const LOCALHOST_SIZE = "http://localhost:3000/".length;
 
 socket.on("connect", () => {
@@ -36,18 +34,31 @@ On Server game message
 */
 socket.on("server-game-message", (msg) => {
     console.log("MESSAGE FROM THE SERVER RELATIVE TO THE GAME:");
-    console.log(msg);
-
+    // console.log(msg);
     outputMessage(msg);
+    document.querySelector("#sandbox_message_box").scrollTop =
+        document.querySelector("#sandbox_message_box").scrollHeight;
 });
 
 const outputMessage = (msg) => {
+    console.log(msg);
     const div = document.createElement("div");
-    const pSpanName = document.createElement("span");
-    const messageContent = document.createElement("p");
+    let pSpanName = document.createElement("span");
+    let messageContent = document.createElement("p");
     div.classList.add("p-2");
-    pSpanName = msg.
+    pSpanName.classList.add("inline");
+    messageContent.classList.add("inline");
+    pSpanName.classList.add("font-bold", "inline", `text-${randomColor()}-700`);
+    pSpanName.textContent = `${msg.display_name} `;
+    messageContent.textContent = msg.message;
+    div.append(pSpanName);
+    div.append(messageContent);
+    document.querySelector("#sandbox_message_box").appendChild(div);
+};
 
+const randomColor = () => {
+    const colors = ["sky", "green", "red", "yellow"];
+    return colors[Math.floor(Math.random() * 3)];
 };
 
 /*
