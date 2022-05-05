3e0539c4fdf48bc66c56f7fcf9e7fc9b47c935ee
PianomanEric <erickristopherfalk@gmail.com>
2022-05-04 19:50:05 -0700

    Started working on game render code

--- public/javascripts/test_game.js
+++ public/javascripts/test_game.js
@@ -1,5 +1,8 @@
 // const { array } = require("joi");
 // const { createCardStateRowsAndCardsRows } = require("../../controller/db_engine_game_uno");
+
+const { socketIDRoomIndex } = require("../../server/constants");
+
 // TODO: FIX DISABLE ON not TURN  
 class Draggable {
 
@@ -189,7 +192,82 @@ draggable.setCallback((parentContainer) => {
     }
 });
 
-class UnoGame {
-    
+// GetHand structure
+/*
+    "Cards".game_id,
+    "CardInfo".type AS card_info_type,
+    "CardInfo".color AS card_color,
+    "CardInfo".content AS card_content,
+    "Collection".player_id,
+    "Collection".collection_index,
+    "Collection".card_state_id,
+    "Collection".collection_info_id,
+    "CardState".card_info_id,
+    "CollectionInfo".type AS collection_info_type
+*/
+
+class UnoGameRenderer {
+    constructor (viewerPlayerId) {
+        this.viewerPlayerId = viewerPlayerId;
+        this.handContainers = {};
+        this.hands = {};        // [player_id] = [element, element, element]
+        this.drawPile = [];     // [element, element, element]
+        this.playPile = [];     // [element, element, element]
+    }
+
+    addPlayer(userId, container) {
+        this.handContainer[userId] = container
+        this.hands[userId] = {};
+    }
+
+    playCard(cardId) {
+        // Post request with card id
+    }
+
+    updateHand(playerId, hand) {
+
+    }
+
+    updateGameState(gameState) {
+        // Get play pile cards, update on callback?
+        // Get my hand cards, update on callback?
+
+        // Do we need to do anything with the draw pile?
+        
+        // Iterate through player hands
+        gameState.players.forEach((player) => {
+            // If new card, add (animate from draw pile later)
+            for (let i = player.collection.length; i < this.hands[player.player_id].length; i++) {
+                // Add card to hand
+            }
+            // If missing card, move to play pile (animate later)
+            for (let i = this.hands[player.player_id].length; i < player.collection.length; i++) {
+                // Reparent card to play pile
+            }
+        });
+
+
+    }
+
+    startTurn() {
+        //let myHand = 
+
+        // Make draw stack draggable
+        //if () {
+            
+        //}
+
+        // Make my cards draggable
+
+        // Hook up draggable callback to play card?
+            // Submit move and end turn
+    }
+
+    endTurn() {
+        // Make cards no longer draggable
+    }
+
 }
 
+// getPlayers()
+//    [id, id, id, id]
