8e351d41933887cadde79a630378ba848d806526
Eddy Yun <eyun1@mail.sfsu.edu>
2022-05-11 15:37:57 -0700

    render start button for only host

--- public/javascripts/logout.js
+++ public/javascripts/logout.js
@@ -1,8 +1,5 @@
 const logout = document.getElementById("logout");
 logout.addEventListener("click", async () => {
-    // await axios.post("/createGame", {}).then((response) => {
-    //     window.location = response.url;
-    // });
     await fetch("/logout", { method: "POST" }).then(
         (response) => (window.location = response.url)
     );
