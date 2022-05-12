a83cf4a38093d9232fbe6f0653bde16e5f4a70a8
Eddy Yun <eyun1@mail.sfsu.edu>
2022-05-12 16:45:42 -0700

    modularized and fixed all html calls into their respective js files, fixed start button (render for only host), fix conflicts with backend

--- public/javascripts/leave_game.js
+++ public/javascripts/leave_game.js
@@ -1,5 +1,11 @@
-const leaveGame = document.getElementById('leaveGame');
-leaveGame.addEventListener('click', () => {
-    axios.post(`${getGameId()}/leaveGame`, {});
-    window.location.pathname = '/';
-});
+const leaveGame = document.getElementById("leaveGame");
+if (leaveGame) {
+    leaveGame.addEventListener("click", () => {
+        axios
+            .post(`${getGameId()}/leaveGame`, {})
+            .then((response) => {
+                window.location.pathname = "/";
+            })
+            .catch((err) => console.log(`Leaving game error: ${err}`));
+    });
+}
--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,44 +1,34 @@
-const login = document.getElementById('login_form_button');
+const login = document.getElementById("login_form_button");
+if (login) {
+    login.addEventListener("click", async (event) => {
+        event.preventDefault();
 
-login.addEventListener('click', async (event) => {
-    event.preventDefault();
+        const formLogin = document.getElementById("login_form");
+        const formData = new FormData(formLogin);
 
-    const formLogin = document.getElementById('login_form');
-    const formData = new FormData(formLogin);
+        await axios
+            .post("/login", Object.fromEntries(formData))
+            .then((response) => {
+                window.location = response.data.url;
+            })
+            .catch((err) => console.log(`Login: ${err}`));
 
-    // for (const value of formData.entries()) {
-    //     console.log(value);
-    // }
-
-    // AXIOS WAY
-    const response = await axios.post(
-        '/login',
-        Object.fromEntries(formData),
-        // {
-        //     headers:
-        //         { accept: 'application/json',
+        // FETCH WAY
+        // const data = new URLSearchParams(new FormData(formLogin));
         //
-        //         },
-        // },
-    ); // TODO: GUARD AND CLEANUP
-    // console.log(response.data)
-    window.location = response.data.url;
-
-    // FETCH WAY
-    // const data = new URLSearchParams(new FormData(formLogin));
-    //
-    // console.log(Object.fromEntries(formData));
-    // await fetch('/login', {
-    //     method: 'POST',
-    //     body: data,
-    //     // mode: 'cors', // no-cors, *cors, same-origin
-    //     // credentials: 'same-origin', // include, *same-origin, omit
-    //     headers: {
-    //         // 'content-type': 'text/json',
-    //         'Content-Type': 'application/x-www-form-urlencoded',
-    //     },
-    // })
-    //     .then((response) => {
-    //         console.log(response);
-    //     });
-});
+        // console.log(Object.fromEntries(formData));
+        // await fetch('/login', {
+        //     method: 'POST',
+        //     body: data,
+        //     // mode: 'cors', // no-cors, *cors, same-origin
+        //     // credentials: 'same-origin', // include, *same-origin, omit
+        //     headers: {
+        //         // 'content-type': 'text/json',
+        //         'Content-Type': 'application/x-www-form-urlencoded',
+        //     },
+        // })
+        //     .then((response) => {
+        //         console.log(response);
+        //     });
+    });
+}
